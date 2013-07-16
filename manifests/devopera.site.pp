307dee345987c3ec6c8fe165c3b8487606a08c73
cleverlight <github.account@cleveraspaint.com>
2013-07-16 20:41:14 +0100

    new version of donagios and some line ending clenaup

--- manifests/devopera.site.pp
+++ manifests/devopera.site.pp
@@ -110,6 +110,7 @@ define process_profile (
     nagios-3: {
       class { 'donagios::server' :
         user => $user,
+        webadmin_limitlocalhost = false,
         require => [Class['dozendserver']],
       }
     }
